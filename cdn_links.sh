1892eae643aa5d94103fed110b484a89fe6193f0
ramiisaac <isaacr@spu.edu>
2024-09-11 22:27:22 -0700

    setup

--- cdn_links.sh
+++ cdn_links.sh
@@ -1,3 +1,10 @@
 find . -type f \( -name "*.js" -o -name "*.css" \) | while read file; do
-  echo "https://cdn.jsdelivr.net/gh/ramiisaac/raisaac@main/${file#./}"
+  filename=$(basename "$file")  # Get the file name
+  if [[ "$file" == *.js ]]; then
+    # Generate <script> tag for JavaScript files
+    echo "<script id=\"${filename%.js}-script\" src=\"https://cdn.jsdelivr.net/gh/ramiisaac/raisaac@main/${file#./}\"></script>"
+  elif [[ "$file" == *.css ]]; then
+    # Generate <link> tag for CSS files
+    echo "<link id=\"${filename%.css}-style\" rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/gh/ramiisaac/raisaac@main@main/${file#./}\">"
+  fi
 done
